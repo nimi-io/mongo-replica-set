78a49a7ec2a726db1282489eb8d2ceab8f7d2e6a
nimi-io <ibukunoluwaodunsi@gmail.com>
2025-09-26 15:40:01 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,4 +8,4 @@ docker-compose down
 echo "Removing MongoDB data volumes..."
 docker volume rm mongo-replica-set_mongo1_data mongo-replica-set_mongo2_data mongo-replica-set_mongo3_data 2>/dev/null || echo "Some volumes may not exist yet"
 
-echo "Cleanup complete!"
+echo "Cleanup-complete!"
